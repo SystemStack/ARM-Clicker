dca54e0140b9b480730b44c48ffb375a2a9be7da
Levi Broadnax <levibroadnax@gmail.com>
2017-05-07 14:49:22 -0500

    Missed file in last PR
    * Adds create user, update email to schema.js
    * Fixes formatting throughout schema.js

--- src/imports/api/schema.js
+++ src/imports/api/schema.js
@@ -5,38 +5,46 @@ export default `
   scalar Date
   # Create User and Click models that reflect our DB schema
   type User {
-    UserID: Int
-    UserName: String
-    Email: String
-    ClickCount: Int
-    Clicks: [Click] # These are all of the Clicks by a user
+    UserID     : Int
+    UserName   : String
+    Email      : String
+    ClickCount : Int
+    Clicks     : [Click] # These are all of the Clicks by a user
   }
 
   # Define a TimeClicked Custom Scalar type
   type Click {
-    ClickID: Int
-    TimeClicked: Date
-    user: User
+    TimeClicked : Date
+    user        : User
   }
 
   # This is the format for the queries in resolvers.js
   type Query {
-    users(UserName: String, Email: String): [User]
-    clicks(UserID: Int): [Click]
+    users(UserName: String, Email: String) : [User]
+    clicks(UserID: Int)                    : [Click]
   }
 
-  # Mutations to increase the click count of a user when a Click is added
-  # @TODO mutation for if a user adds an email after account creation
   type Mutation {
+    createUser (
+      UserName : String,
+      Email    : String
+    ): User
+
+    updateEmail (
+      UserName : String,
+      Email    : String
+    ): User
+
+    # Mutations to increase the click count of a user and
+    # adds a click to the Click table
     incrementClick (
-      ClickID: Int,
-      UserID: Int,
-      TimeClicked: Date
+      UserID      : Int,
+      TimeClicked : Date
     ): Click
   }
 
   schema {
-    query: Query
-    mutation: Mutation
+    query    : Query
+    mutation : Mutation
   }
 `;
