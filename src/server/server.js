c73d30e8ec1bc190c4ae633ec53df90a95694927
Levi Broadnax <levibroadnax@gmail.com>
2017-05-17 02:18:03 -0500

    Users are created in DB on creation
    * Users now have optional emails as well

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,32 @@
 import resolvers from '/imports/api/resolvers';
 import typeDefs from '/imports/api/schema';
+import { AccountsServer } from 'meteor/accounts-base';
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
+
+_createUser = (user) => {
+  // Create a user our MySQL DB using our API
+  // user.emails === [ {address: String, verified: Boolean},...]
+  let _user = {
+    Email: user.emails[0].address,
+    UserName: user.username
+  };
+
+  resolvers.Mutation.createUser(this, _user);
+}
+
+Accounts.onCreateUser((options, user) => {
+  // This middleware is called on registration to be inserted into our MySQL DB
+  _createUser(user);
+  // onCreateUser always expects a user back
+  return user;
+});
+
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
 });
-createApolloServer({
+
+const apolloServer = createApolloServer({
   schema
 });
